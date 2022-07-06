95298369c4151d84a79d5863ca79656639a75a6e
Sebastian Güiza <juanitoguiza@gmail.com>
2022-07-06 08:57:50 -0500

    Load I Am Rich app, my first app in IOS

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  I am Rich
+//
+//  Created by Sebastian Güiza 29-03-22.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,10 @@
+
+I Am Rich, Developed by Sebastian Güiza, using Xcode and App Brewery
+
+#  *** I Am Rich ***
+
+This is I Am Rich, and is the first app that I made to use with iOS
+
+# Enjoy it !!!
+
+![Main Screen](IAmRichScreen.png)
